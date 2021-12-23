f5eced78e9d713b333781466a83a6f1baf5be923
YourBrainEatsYou <me@yourbraineatsyou.ch>
2021-12-23 21:48:53 +0100

    🗨 removed comments

--- src/app/guards/key-exchange.guard.ts
+++ src/app/guards/key-exchange.guard.ts
@@ -1,7 +1,6 @@
-import { KeyExchangeDto } from '@/interfaces/dto/key-exchange-dto';
 import { AuthFacadeService } from '@/store/auth/auth-facade.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, UrlTree, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, UrlTree, Router } from '@angular/router';
 import { Observable, switchMap, of, catchError } from 'rxjs';
 
 @Injectable({
@@ -15,16 +14,11 @@ export class KeyExchangeGuard implements CanActivate {
   ) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, snapshot: RouterStateSnapshot): Observable<boolean | UrlTree> {
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> {
     // exchange hash to access and refresh token
-    console.log(route);
-    console.log(snapshot);
-
-    const keyExchangeDto: KeyExchangeDto = {
+    return this.authFacadeService.postKeyExchange({
       token: route.paramMap.get('token') || ''
-    };
-
-    return this.authFacadeService.postKeyExchange(keyExchangeDto).pipe(
+    }).pipe(
       switchMap(() => of(this.router.createUrlTree(['/']))),
       catchError(() => of(true))
     );
