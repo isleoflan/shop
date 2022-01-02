8035bf7f576249508be0dc4d9135af122907f18b
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-01-02 21:28:26 +0100

    💲 started with the checkout component

--- src/app/pages/shop/checkout/checkout.module.ts
+++ src/app/pages/shop/checkout/checkout.module.ts
@@ -1,3 +1,4 @@
+import { HeaderModule } from '@/components/header/header.module';
 import { CheckoutComponent } from '@/pages/shop/checkout/checkout.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -11,7 +12,8 @@ import { CheckoutRoutingModule } from './checkout-routing.module';
   ],
   imports: [
     CommonModule,
-    CheckoutRoutingModule
+    CheckoutRoutingModule,
+    HeaderModule
   ]
 })
 export class CheckoutModule {
