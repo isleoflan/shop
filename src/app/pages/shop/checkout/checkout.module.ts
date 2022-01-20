fb2255882c0383f09fd7d125fb17c4251eceeb01
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-01-20 02:11:04 +0100

    👤 implemented user data fetch and prefill checkout form

--- src/app/pages/shop/checkout/checkout.module.ts
+++ src/app/pages/shop/checkout/checkout.module.ts
@@ -1,8 +1,13 @@
 import { HeaderModule } from '@/components/header/header.module';
 import { DirectivesModule } from '@/directives/directives.module';
 import { CheckoutComponent } from '@/pages/shop/checkout/checkout.component';
+import { TopUpModule } from '@/pages/shop/top-up/top-up.module';
 import { PipesModule } from '@/pipes/pipes.module';
 import { CartStoreModule } from '@/store/cart/cart-store.module';
+import { CateringStoreModule } from '@/store/catering/catering-store.module';
+import { MerchandiseStoreModule } from '@/store/merchandise/merchandise-store.module';
+import { TicketStoreModule } from '@/store/ticket/ticket-store.module';
+import { UserStoreModule } from '@/store/user/user-store.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -31,6 +36,11 @@ import { VoucherComponent } from './voucher/voucher.component';
     ReactiveFormsModule,
     PipesModule,
     CartStoreModule,
+    UserStoreModule,
+    CateringStoreModule,
+    MerchandiseStoreModule,
+    TicketStoreModule,
+    TopUpModule,
     DirectivesModule
   ]
 })
