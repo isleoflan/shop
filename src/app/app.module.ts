69624fd1ea41e8a7ada3a70ac2848666911e0155
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-01-20 22:59:08 +0100

    🗣 translated shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { AbstractShopApiService } from '@/api/abstract-shop-api.service';
 import { ShopApiService } from '@/api/shop-api.service';
 import { AppStoreModule } from '@/store/app-store.module';
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localCH from '@angular/common/locales/de-CH';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AbstractAuthApiService } from './api/abstract-auth-api.service';
 import { AuthApiService } from './api/auth-api.service';
@@ -12,13 +14,17 @@ import { AppComponent } from './app.component';
 import { httpInterceptorProviders } from './interceptors';
 import { RedirectComponent } from './pages/redirect/redirect.component';
 
+registerLocaleData(localCH);
+
 @NgModule({
   declarations: [AppComponent, RedirectComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, AppStoreModule],
   providers: [
     httpInterceptorProviders,
     { provide: AbstractAuthApiService, useClass: AuthApiService },
-    { provide: AbstractShopApiService, useClass: ShopApiService }
+    { provide: AbstractShopApiService, useClass: ShopApiService },
+    { provide: LOCALE_ID, useValue: 'de-CH' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'CHF' }
   ],
   bootstrap: [AppComponent]
 })
